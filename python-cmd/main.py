35f22c3e8ee49d38ba458593f7546dc18b30cd39
Hironori Yamamoto <hironori.yamamoto@gunosy.com>
2022-09-09 02:39:33 +0900

    :sparkles: support wheel builds

--- python-cmd/main.py
+++ python-cmd/main.py
@@ -1,9 +1,8 @@
-import pyo3_pkg
-from pythonlib import vector
+from pythonlib import vector, rust_echo, golib
 
 def main():
-    pyo3_pkg.rust_echo("Hello from Python!")
-    pyo3_pkg.golib.go_echo("Hello from Python!")
+    rust_echo("Hello from Python!")
+    golib.go_echo("Hello from Python!")
     print("Call internal another package: ", vector.create_vector([1, 2, 3]))
 
 if __name__ == "__main__":
---
+++ pythonlib/pythonlib/__init__.py
@@ -0,0 +1,6 @@
+from pyo3_pkg import rust_echo, golib
+
+__all__ = [
+    "golib",
+    "rust_echo",
+]
